3f90c12c0ebfee4de5a35fc4885b0f982242a471
anashkevichp <anashkevichp@gmail.com>
2014-06-24 04:02:02 +0300

    network logic  global edit

--- CurrencyInformation/CIMainTableViewController.m
+++ CurrencyInformation/CIMainTableViewController.m
@@ -14,10 +14,18 @@
 #import "CIBank.h"
 
 #define JSON_URL @"http://wm.shadurin.com/select.php"
-#define REQUEST_TIMEOUT_INTERVAL 7.0
+
+#define REQUEST_TIMEOUT_INTERVAL 7.0    // request time interval in seconds
+
+#define UPDATE_PAUSE 3                  // update pause in hours
+
 #define BANK_DETAILS_PLIST_NAME @"bankDetails"
 #define CURRENCY_RATES_PLIST_NAME @"currencyRates.plist"
 
+#define NO_CONNECTION 0
+#define UNKNOWN_CONNECTION -1
+#define WWAN_CONNECTION 1
+#define WIFI_CONNECTION 2
 
 @interface CIMainTableViewController ()
 
@@ -42,17 +50,69 @@
 {
     [super viewDidLoad];
     
+    banks = [NSMutableArray arrayWithCapacity:10];
     [self.tableView setUserInteractionEnabled:NO];
+    [self readBankDetailsFromPlist];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ReloadNotification:) name:@"SaveToPlist" object:nil];
+    
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
+    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        switch (status) {
+            case NO_CONNECTION:
+                NSLog(@"connection off!");
+                break;
+            case WWAN_CONNECTION:
+            case WIFI_CONNECTION:
+            {
+                [[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] setUserInteractionEnabled:YES];
+                [[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] textLabel] setTextColor:[UIColor blackColor]];
+                
+                NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+                NSString *documentsDir = [paths objectAtIndex:0];
+                NSString *filePath = [documentsDir stringByAppendingPathComponent:CURRENCY_RATES_PLIST_NAME];
+                
+                NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filePath error:nil];
+                
+                
+                if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+                    NSDate *fileModifDate = [fileAttributes fileModificationDate];
+                    NSDateComponents *curComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSHourCalendarUnit fromDate:[NSDate date]];
+                    NSDateComponents *fileModifComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSHourCalendarUnit fromDate:fileModifDate];
+                    
+                    if ([curComponents day] == [fileModifComponents day]) {
+                        if (([curComponents hour] - [fileModifComponents hour]) >= UPDATE_PAUSE) {
+                            [self networkingOperation];
+                        } else {
+                            ratesDict = [NSMutableDictionary dictionaryWithContentsOfFile:filePath];
+                            [self mergeDictionaries];
+                            [self.tableView setUserInteractionEnabled:YES];
+                        }
+                    } else {
+                        [self networkingOperation];
+                    }
+                } else {
+                    [self networkingOperation];
+                }
+            }
+                break;
+            default:
+                break;
+        }
+    }];
+}
+
+- (void) networkingOperation
+{
+    NSLog(@"Networking operation started!");
+    
+    banks = [NSMutableArray arrayWithCapacity:10];
+    
     UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
     [indicator setCenter:self.tableView.center];
     [indicator setHidesWhenStopped:YES];
     [indicator startAnimating];
     [self.view addSubview:indicator];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ReloadNotification:) name:@"SaveToPlist" object:nil];
-    
-    banks = [NSMutableArray arrayWithCapacity:10];
-    [self readBankDetailsFromPlist];
     
     /*              *** start networking ***              */
     
@@ -70,26 +130,42 @@
         [[NSNotificationCenter defaultCenter] postNotificationName:@"SaveToPlist" object:self];
         
         [indicator stopAnimating];
+        [[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] setUserInteractionEnabled:YES];
+        [[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] textLabel] setTextColor:[UIColor blackColor]];
         [self.tableView setUserInteractionEnabled:YES];
         
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        UIAlertView *connectionErrorAlert = [[UIAlertView alloc] initWithTitle:@"Ошибка получения данных!"
-                                                            message:@"Будут использованы ранее сохраненные данные"
-                                                           delegate:nil
-                                                  cancelButtonTitle:@"Продолжить"
-                                                  otherButtonTitles:nil];
+        [[self tableView] reloadData];
+        [[self updateDataAlert] show];
         
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
         NSString *documentsDir = [paths objectAtIndex:0];
         NSString *filePath = [documentsDir stringByAppendingPathComponent:CURRENCY_RATES_PLIST_NAME];
         
-        ratesDict = [NSMutableDictionary dictionaryWithContentsOfFile:filePath];
-        [self mergeDictionaries];
-        
-        [indicator stopAnimating];
-        [self.tableView setUserInteractionEnabled:YES];
-        [connectionErrorAlert show];
+        if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+            
+            ratesDict = [NSMutableDictionary dictionaryWithContentsOfFile:filePath];
+            [self mergeDictionaries];
+            [indicator stopAnimating];
+            [self.tableView setUserInteractionEnabled:YES];
+            [[self connectionErrorAlert] show];
+            
+        } else {
+            [self mergeDictionaries];
+            [indicator stopAnimating];
+            [[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] setUserInteractionEnabled:YES];
+            [[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] textLabel] setTextColor:[UIColor blackColor]];
+            [self.tableView setUserInteractionEnabled:YES];
+            
+            [[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] setUserInteractionEnabled:NO];
+            [[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] textLabel] setTextColor:[UIColor grayColor]];
+            
+            UIAlertView *alertView = [self connectionErrorAlert];
+            [alertView setMessage:@"Для просмотра курсов валют потребуется подключение к интернету!"];
+            [alertView show];
+        }
+        [[self tableView] reloadData];
     }];
     
     [operation start];
@@ -97,7 +173,27 @@
     /*                *** end networking ***              */
 }
 
-- (void)ReloadNotification:(NSNotification *)notification
+- (UIAlertView *) connectionErrorAlert
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Ошибка получения данных!"
+                                                        message:@"Будут использованы ранее сохраненные данные."
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Продолжить"
+                                              otherButtonTitles:nil];
+    return alertView;
+}
+
+- (UIAlertView *) updateDataAlert
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Успешное подключение к интернету!"
+                                                        message:@"Данные обновлены."
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Продолжить"
+                                              otherButtonTitles:nil];
+    return alertView;
+}
+
+- (void) ReloadNotification:(NSNotification *)notification
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
     NSString *documentsDir = [paths objectAtIndex:0];
